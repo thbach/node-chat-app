ad362a24e93ba64b04fdabafe3b16d98ea1a4a04
tbach <thbach@gmail.com>
2018-09-18 23:33:01 -0700

    added form elements

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,19 @@ socket.on('disconnect', () => {
   console.log('Disconnected from Server');
 });
 
-socket.on('newMessage', newMessage => {
-  console.log('newMessage', newMessage);
+socket.on('newMessage', message => {
+  console.log('newMessage', message);
+  const li = document.createElement('li');
+  const textNode = document.createTextNode(`${message.from}: ${message.text}`);
+  li.appendChild(textNode);
+  document.getElementById('messages').appendChild(li);
+});
+
+const form = document.getElementById('message-form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const msg = document.getElementById('message').value;
+  socket.emit('createMessage', {from: 'tbach', text: msg}, data => {
+    console.log('Got it', data);
+  });
 });
